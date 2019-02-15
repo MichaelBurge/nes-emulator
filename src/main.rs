239ac20838c0511a6aa7ed8e6bdc9bb14797f24c
Michael Burge <michaelburge@pobox.com>
2019-02-15 15:52:18 -0800

    Render from an SDL surface to a window canvas, using textures.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
 extern crate sdl2;
 
+use sdl2::pixels::PixelFormatEnum;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::render::Canvas;
 use sdl2::surface::Surface;
+use sdl2::video::Window;
 use std::time::Duration;
 
 // https://wiki.nesdev.com/w/index.php/Cycle_reference_chart
-const CLOCKS_PER_FRAME = 29780;
+const CLOCKS_PER_FRAME:u32 = 29780;
 
 fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -18,7 +21,7 @@ fn main() {
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
-    let mut render_surface = Surface::new(RENDER_WIDTH, RENDER_HEIGHT, PixelFormatEnum::Index8);
+    let mut render_surface = Surface::new(RENDER_WIDTH, RENDER_HEIGHT, PixelFormatEnum::Index8).unwrap();
     let nes = create_nes();
 
     canvas.set_draw_color(Color::RGB(0, 255, 255));
@@ -41,7 +44,7 @@ fn main() {
         }
         // The rest of the game loop goes here...
         render_frame(nes, render_surface);
-        render_surface.blit(None, canvas.surface_mut(), None);
+        present_frame(canvas, &render_surface);
 
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
@@ -55,6 +58,12 @@ fn create_nes() -> Nes {
 
 fn render_frame(nes: Nes, surface: Surface) {
     run_clocks(nes, 29780);
-    let (Some(buffer)) = surface.without_lock_mut();
+    let Some(buffer) = surface.without_lock_mut();
     nes.ppu.render(buffer);
+}
+
+fn present_frame(canvas: Canvas<Window>, surface: &Surface) {
+    let texture_creator = canvas.texture_creator();
+    let texture = texture_creator.create_texture_from_surface(surface).unwrap();
+    canvas.copy(&texture, None, None);
 }
